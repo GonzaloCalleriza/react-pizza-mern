6e8a07b0680031e1b64dad9e0b9527ce2f13068c
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-03-03 13:56:08 +0100

    Finished creating Paypal functionality, starting on cash on site

---
+++ pages/api/orders/[id].js
@@ -0,0 +1,24 @@
+import dbConnect from '../../../util/mongo';
+import Order from '../../../models/Order';
+
+const handler = async (req, res) => {
+    const {method, query:{id}} = req;
+
+    await dbConnect();
+
+    if(method === 'GET'){
+        try {
+            const order = await Order.findById(id);
+            res.status(201).json(order)
+
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
+
+    if(method === 'PuT'){}
+
+    if(method === 'DELETE'){}
+};
+
+export default handler;
